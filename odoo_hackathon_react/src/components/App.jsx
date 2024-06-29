4d5a722c698dde3a887a88bf666afe3fc79fb3e5
MStrange11 <gta5andepic@gmail.com>
2024-06-29 13:25:11 +0530

    navbar complete

--- odoo_hackathon_react/src/components/App.jsx
+++ odoo_hackathon_react/src/components/App.jsx
@@ -2,14 +2,20 @@
 import './App.css';
 import Login from "./Login.js"
 import Navbar from "./navbar.js"
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
-    <div>
+    // <Router>
+
+    <div className='App'>
       <Navbar/>
-      <Login/>
+      {/* <Routes> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      <Login />
+      {/* </Routes> */}
     </div>
+    // </Router>
   );
 }
 
